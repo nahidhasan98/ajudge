fee09e2579676521acae2d31dffac73aa95ed76d
Nahid Hasan <mnh.nahid35@gmail.com>
2021-06-29 18:12:07 +0600

    this is test file, not necessary

--- backend/test.go
+++ backend/test.go
@@ -2,139 +2,27 @@ package backend
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"time"
 
-	"github.com/nahidhasan98/ajudge/model"
+	"github.com/nahidhasan98/ajudge/oj/toph"
 )
 
 //Test function for testing a piece of code
 func Test(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	// contestDateTime := "324242:00"
-	// _, err := time.Parse(time.RFC3339, contestDateTime) //format RFC3339 = "2006-01-02T15:04:05Z07:00"
+	apiURL := "https://toph.co/problems/all?start=1"
+	response := toph.GETRequest(apiURL)
+	fmt.Println(response)
+	defer response.Body.Close()
+	// _, err := goquery.NewDocumentFromReader(response.Body)
 	// errorhandling.Check(err)
 
-	location, _ := time.LoadLocation("Asia/Singapore")
-	currentTime := time.Now().In(location)
-	zone, off := currentTime.Zone()
-	// contestDateTime := "2021-01-02" + "T" + "15:04" + ":00" + zone
-	// contestDT, err := time.Parse(time.RFC3339, contestDateTime) //format RFC3339 = "2006-01-02T15:04:05Z07:00"
-	// errorhandling.Check(err)
-	fmt.Println(zone, off/60, currentTime)
+	resp, _ := ioutil.ReadAll(response.Body)
+	body := string(resp)
+	fmt.Println("Hellooooo", body)
 
-	location, _ = time.LoadLocation("UTC")
-	currentTime = time.Now().In(location)
-	zone, off = currentTime.Zone()
-	// contestDateTime := "2021-01-02" + "T" + "15:04" + ":00" + zone
-	// contestDT, err := time.Parse(time.RFC3339, contestDateTime) //format RFC3339 = "2006-01-02T15:04:05Z07:00"
-	// errorhandling.Check(err)
-	fmt.Println(zone, off/60, currentTime)
-
-	currentTime = time.Now()
-	zone, off = currentTime.Zone()
-	// contestDateTime := "2021-01-02" + "T" + "15:04" + ":00" + zone
-	// contestDT, err := time.Parse(time.RFC3339, contestDateTime) //format RFC3339 = "2006-01-02T15:04:05Z07:00"
-	// errorhandling.Check(err)
-	fmt.Println(zone, off/60, currentTime)
-
-	// currentUnix := time.Now().Unix()
-	// zone, off = currentTime.Zone()
-	// // contestDateTime := "2021-01-02" + "T" + "15:04" + ":00" + zone
-	// // contestDT, err := time.Parse(time.RFC3339, contestDateTime) //format RFC3339 = "2006-01-02T15:04:05Z07:00"
-	// // errorhandling.Check(err)
-	// fmt.Println(zone, off)
-	// fmt.Println(currentTime)
-	// fmt.Println(currentUnix)
-
-	// t := time.Unix(1614346849, 0)
-	// fmt.Println(t)
-
-	// tl := time.Now().Local()
-	// fmt.Println(tl)
-
-	//not found: !=nil - mongo: no document in result
 	fmt.Println("ENDDDDDD")
-	model.Tpl.ExecuteTemplate(w, "test.html", nil)
+	//model.Tpl.ExecuteTemplate(w, "test.html", nil)
 }
-
-//URISearch function for searching problem in URI
-// func collectProbList(w http.ResponseWriter) []byte {
-// 	defer recovery() //for panic() error recovery
-
-// 	//defining a variable for returning data
-// 	var res []byte
-
-// 	//data that will be collected
-// 	type list struct {
-// 		Num, Name string
-// 	}
-// 	var problemList []list
-
-// 	//two separate lists(number & name) of problem. will be entered in final problem list later
-// 	var probNumList, probNameList []string
-// 	var tempNum, tempName string
-// 	var uriPageNumList []int
-
-// 	fmt.Println(time.Now())
-// 	for k := 1; k <= 85; k++ { //taking 10 pages problem
-// 		var apiURL string
-
-// 		apiURL = "https://www.urionlinejudge.com.br/judge/en/problems/all?page=" + strconv.Itoa(k)
-
-// 		response := URIGETRequest(apiURL)
-// 		defer response.Body.Close()
-// 		document, err := goquery.NewDocumentFromReader(response.Body)
-// 		CheckErr(err)
-
-// 		//collecting a list of problem numbers
-// 		document.Find("td[class='large']").Each(func(index int, mixedStr *goquery.Selection) {
-// 			tempNum, _ = mixedStr.Find("a").Attr("href")
-
-// 			need := "/judge/en/problems/view/"
-// 			match := strings.Index(tempNum, need)
-
-// 			if match != -1 {
-// 				tempNum = strings.TrimPrefix(tempNum, "/judge/en/problems/view/")
-// 				probNumList = append(probNumList, tempNum)
-
-// 				tempName = mixedStr.Find("a").Text()
-// 				probNameList = append(probNameList, tempName)
-
-// 				uriPageNumList = append(uriPageNumList, k)
-// 			}
-// 		})
-// 	}
-// 	fmt.Println(time.Now())
-// 	DB := dbConn()
-// 	defer DB.Close()
-
-// 	//preparing final list from two separate lists
-// 	for i := 0; i < len(probNumList); i++ {
-// 		temp := list{probNumList[i], probNameList[i]}
-// 		problemList = append(problemList, temp)
-
-// 		fmt.Fprintln(w, probNumList[i], probNameList[i])
-
-// 		var id int
-// 		exist := DB.QueryRow("SELECT id FROM uri WHERE pNum=?", probNumList[i]).Scan(&id)
-
-// 		if exist == sql.ErrNoRows {
-// 			insertQuery, err := DB.Prepare("INSERT INTO uri (pNum,pName,uriPageNum) VALUES (?,?,?)")
-// 			CheckErr(err)
-// 			insertQuery.Exec(probNumList[i], probNameList[i], uriPageNumList[i])
-// 		}
-// 	}
-// 	fmt.Println(time.Now())
-// 	res, _ = json.Marshal(problemList) ///making json file
-
-// 	return res
-// }
-
-// subIDs := [2]int{4, 6}
-// //setting uo options for retrieving data from DB
-// opts := options.Find()
-// opts.SetSort(bson.D{{Key: "subID", Value: -1}}) //descending order
-// cursor, err := submissionCollection.Find(ctx, bson.M{"subID": bson.M{"$in": subIDs}}, opts)
-// CheckErr(err)
