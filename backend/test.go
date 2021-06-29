65630e92b7d5f54300194629419cb2af6773ac1f
Nahid Hasan <mnh.nahid35@gmail.com>
2021-06-30 02:04:53 +0600

    test code edited

--- backend/test.go
+++ backend/test.go
@@ -4,25 +4,152 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 
-	"github.com/nahidhasan98/ajudge/oj/toph"
+	"github.com/nahidhasan98/ajudge/errorhandling"
+	"github.com/nahidhasan98/ajudge/model"
 )
 
 //Test function for testing a piece of code
 func Test(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	apiURL := "https://toph.co/problems/all?start=1"
-	response := toph.GETRequest(apiURL)
-	fmt.Println(response)
-	defer response.Body.Close()
-	// _, err := goquery.NewDocumentFromReader(response.Body)
-	// errorhandling.Check(err)
+	for key := range model.OJSet {
+		fmt.Println(key)
+		if key == "计蒜客" || key == "黑暗爆炸" {
+			key = url.QueryEscape(key)
+		}
+		apiURL := "https://vjudge.net/problem/data?draw=1&start=0&length=1&sortDir=desc&sortCol=5&OJId=" + key + "&probNum=&title=&source=&category=all"
 
-	resp, _ := ioutil.ReadAll(response.Body)
-	body := string(resp)
-	fmt.Println("Hellooooo", body)
+		req, err := http.NewRequest("GET", apiURL, nil)
+		errorhandling.Check(err)
+		req.Header.Add("Content-Type", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		response, err := model.Client.Do(req)
+		errorhandling.Check(err)
+		defer response.Body.Close()
+
+		body, _ := ioutil.ReadAll(response.Body)
+
+		fmt.Fprintln(w, string(body))
+	}
 
 	fmt.Println("ENDDDDDD")
 	//model.Tpl.ExecuteTemplate(w, "test.html", nil)
 }
+
+/*
+	//searching problem from DimikOJ first
+	var pRes []byte
+	if OJ == "" || OJ == "All" || OJ == "DimikOJ" {
+		if pNum != "" {
+			pRes = dimik.Search(pNum)
+		} else if pName != "" {
+			pRes = dimik.Search(pName)
+		} else {
+			if OJ == "DimikOJ" { //again checking cause it may contains only DimikOJ or All OJ
+				pRes = dimik.Search("DimikOJ Only")
+			} else {
+				rand.Seed(time.Now().UnixNano())
+				min := 1   //the first problem DimikOJ is 1
+				max := 100 //the last problem DimikOJ is approx. 100
+				sQuery := rand.Intn(max-min+1) + min
+				pRes = dimik.Search(strconv.Itoa(sQuery))
+			}
+		}
+		type PResList struct {
+			Num  string `json:"Num"`
+			Name string `json:"Name"`
+		}
+		var pResList []PResList
+		json.Unmarshal(pRes, &pResList)
+
+		for i := 0; i < len(pResList); i++ {
+			var temp model.List
+
+			temp.OJ = "DimikOJ"
+			temp.Num = pResList[i].Num
+			temp.Name = pResList[i].Name
+
+			pListFinal = append(pListFinal, temp)
+		}
+	}
+	if OJ == "" || OJ == "All" || OJ == "Toph" {
+		if pNum != "" {
+			pRes = toph.Search(pNum)
+		} else if pName != "" {
+			pRes = toph.Search(pName)
+		} else {
+			if OJ == "Toph" { //again checking cause it may contains only DimikOJ or All OJ
+				pRes = toph.Search("Toph Only")
+			} else { //actually this do nothing because Toph has no problem number
+				rand.Seed(time.Now().UnixNano())
+				min := 1   //the first problem DimikOJ is 1
+				max := 100 //the last problem DimikOJ is approx. 100
+				sQuery := rand.Intn(max-min+1) + min
+				pRes = toph.Search(strconv.Itoa(sQuery))
+			}
+		}
+		type PResList struct {
+			Num  string `json:"Num"`
+			Name string `json:"Name"`
+		}
+		var pResList []PResList
+		json.Unmarshal(pRes, &pResList)
+
+		for i := 0; i < len(pResList); i++ {
+			var temp model.List
+
+			temp.OJ = "Toph"
+			temp.Num = pResList[i].Num
+			temp.Name = pResList[i].Name
+
+			pListFinal = append(pListFinal, temp)
+		}
+	}
+	if OJ == "" || OJ == "All" || OJ == "URI" {
+		if pNum != "" {
+			pRes = uri.Search(pNum)
+		} else if pName != "" {
+			pRes = uri.Search(pName)
+		} else {
+			if OJ == "URI" {
+				pRes = uri.Search("URI Only")
+			} else {
+				rand.Seed(time.Now().UnixNano())
+				min := 1001 //the first problem in URI is 1001
+				max := 3100 //the last problem in URI is approx. 3100
+				sQuery := rand.Intn(max-min+1) + min
+				pRes = uri.Search(strconv.Itoa(sQuery))
+			}
+		}
+		type PResList struct {
+			Num  string `json:"Num"`
+			Name string `json:"Name"`
+		}
+		var pResList []PResList
+		json.Unmarshal(pRes, &pResList)
+
+		for i := 0; i < len(pResList); i++ {
+			var temp model.List
+
+			temp.OJ = "URI"
+			temp.Num = pResList[i].Num
+			temp.Name = pResList[i].Name
+
+			pListFinal = append(pListFinal, temp)
+		}
+	}
+	if OJ != "DimikOJ" && OJ != "Toph" && OJ != "URI" {
+		probData := vjudge.Search(OJ, pNum, pName)
+
+		var temp model.List
+		for i := 0; i < len(probData); i++ {
+			temp.OJ = probData[i].OJ
+			temp.Num = probData[i].Num
+			temp.Name = probData[i].Name
+
+			pListFinal = append(pListFinal, temp)
+		}
+	}
+
+*/
