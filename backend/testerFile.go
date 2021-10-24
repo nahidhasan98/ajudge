9e9408e0be9ff6a890030911cf29c896f24b4057
Nahid Hasan <mnh.nahid35@gmail.com>
2021-10-24 11:28:18 +0600

    test file edited

--- backend/testerFile.go
+++ backend/testerFile.go
@@ -3,16 +3,11 @@ package backend
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/nahidhasan98/ajudge/mail"
 )
 
 //Test function for testing a piece of code
 func Test(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	m := mail.Init()
-	m.SendMailForPasswordReset("mnh.nahid35@gmail.com", "nahidhasan98", "https://ajudge.net")
 
 	fmt.Println("ENDDDDDD")
 	fmt.Println("Happy coding.")
